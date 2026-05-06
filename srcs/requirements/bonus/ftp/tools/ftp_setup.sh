219257c73b7a91294c9dddb8100aea161b71290f
Alexandre <alexandredeabreu711@gmail.com>
2026-05-06 22:05:57 +0200

    fix: fixed protection

--- srcs/requirements/bonus/ftp/tools/ftp_setup.sh
+++ srcs/requirements/bonus/ftp/tools/ftp_setup.sh
@@ -3,13 +3,11 @@
 #secret
 FTP_PASS=$(cat /run/secrets/ftp_password)
 
-if [! id "$FTP_USER" &>/dev/null;] then
-	# Créer le user FTP
-	useradd -m ${FTP_USER}
-	echo "${FTP_USER}:${FTP_PASS}" | chpasswd
+# Créer le user FTP
+useradd -m ${FTP_USER}
+echo "${FTP_USER}:${FTP_PASS}" | chpasswd
 
-	# Donner accès au dossier wordpress
-	chown -R ${FTP_USER}:${FTP_USER} /var/www/wordpress
-fi
+# Donner accès au dossier wordpress
+chown -R ${FTP_USER}:${FTP_USER} /var/www/wordpress
 
 vsftpd /etc/vsftpd.conf
