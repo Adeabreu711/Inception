19b0de7074e45660eebc20ca4fae9d2065e9ea34
alde-abr <alexandredeabreu711@gmail.com>
2026-05-06 18:06:36 -0400

    fix : fixed mariadb correction

--- srcs/requirements/mariadb/tools/mdb_setup.sh
+++ srcs/requirements/mariadb/tools/mdb_setup.sh
@@ -8,18 +8,16 @@ sleep 3
 MYSQL_PASSWORD=$(cat /run/secrets/db_password)
 MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
-if [ ! -d /var/lib/mysql/$MYSQL_DB ]; then
-	#config
-	mysqladmin -u root password ${MYSQL_ROOT_PASSWORD}
+#config
+mysqladmin -u root password ${MYSQL_ROOT_PASSWORD}
 
-	mariadb -u root -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
-	mariadb -u root -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-	mariadb -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \`${MYSQL_USER}\`@'%';"
-	mariadb -u root -e "FLUSH PRIVILEGES;"
+mariadb -u root -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
+mariadb -u root -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mariadb -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \`${MYSQL_USER}\`@'%';"
+mariadb -u root -e "FLUSH PRIVILEGES;"
 
-	# Shutdown mariadb to restart with new config
-	mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
-fi
+# Shutdown mariadb to restart with new config
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 
 # Restart mariadb with new config in the background to keep the container running
 exec mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
